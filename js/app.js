02fe2ac5cca0bc5980ce32d79b26a5119592bd26
meraj191-35-2735 <www.shamsad35-2735@diu.edu.bd>
2022-02-17 03:27:21 +0600

    error handler done

--- js/app.js
+++ js/app.js
@@ -1,32 +1,37 @@
+// Function for total Expense
 function totalExpense() {
     const foodText = document.getElementById('food').value;
     const food = parseFloat(foodText);
 
-
     const rentText = document.getElementById('rent').value;
     const rent = parseFloat(rentText);
-
 
     const clothText = document.getElementById('cloth').value;
     const cloth = parseFloat(clothText);
 
+    const salaryText = document.getElementById('salary').value;
+    const salary = parseFloat(salaryText);
 
     if (typeof food == "number" && typeof rent == "number" && typeof cloth == "number") {
         if (food > 0 && rent > 0 && cloth > 0) {
             document.getElementById('alert').innerText = "";
             const expenses = document.getElementById('total-expense');
             const totalExpense = food + rent + cloth;
-            expenses.innerText = totalExpense;
+            if (totalExpense <= salary) {
+                expenses.innerText = totalExpense;
+            } else {
+                document.getElementById('alert').innerText = 'Insufficient Balance!';
+            }
             return totalExpense;
-
         } else {
-            document.getElementById('alert').innerText = "Give number greater than 0";
+            document.getElementById('alert').innerText = "Invalid Input!";
         }
     } else {
-        document.getElementById('alert').innerText = "Give number type data";
+        document.getElementById('alert').innerText = "Invalid Input!";
     }
 }
 
+// function for Balance
 function balance() {
     const salaryText = document.getElementById('salary').value;
     const salary = parseFloat(salaryText);
@@ -38,13 +43,14 @@ function balance() {
             balance.innerText = restMoney;
             return restMoney;
         } else {
-            document.getElementById('alert').innerText = "Give number greater than 0";
+            document.getElementById('alert').innerText = "Invalid Input!";
         }
     } else {
-        document.getElementById('alert').innerText = "Give number type data";
+        document.getElementById('alert').innerText = "Invalid Input!";
     }
 }
 
+// function for identify saving amount of salary
 function percentageAmount() {
     const savePercentageText = document.getElementById('save-percentage');
     const savePercentage = parseFloat(savePercentageText.value);
@@ -55,25 +61,30 @@ function percentageAmount() {
         const salary = parseFloat(salaryText);
         const saveAmount = salary * percentageAmount;
         const saveAmountText = document.getElementById('save-amount');
-        saveAmountText.innerText = saveAmount;
+        if (saveAmount <= balance()) {
+            saveAmountText.innerText = saveAmount;
+        } else {
+            document.getElementById('second-alert').innerText = 'Insufficient Balance!';
+        }
         return saveAmount;
     } else {
-        document.getElementById('second-alert').innerText = "Invalid Input";
+        document.getElementById('second-alert').innerText = "Invalid Input!";
     }
 }
 
+// function for Remaining Balance
 function remainingBalance() {
     const remainingBalance = document.getElementById('rest-balance');
     const restBalance = balance() - percentageAmount();
     remainingBalance.innerText = restBalance;
 }
-
+// add event handler to button
 document.getElementById('calculate-button').addEventListener('click', function() {
-    totalExpense();
-    balance();
+    totalExpense(); //call the totalExpense function
+    balance(); //call the balance function
 })
 
 document.getElementById('save').addEventListener('click', function() {
-    percentageAmount();
-    remainingBalance();
+    percentageAmount(); //call the percentageAmount function
+    remainingBalance(); //call the remaining balance function
 })
