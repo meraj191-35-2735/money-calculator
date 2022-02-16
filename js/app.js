dec184fe0f9ad466f61343034195faaadf498714
meraj191-35-2735 <www.shamsad35-2735@diu.edu.bd>
2022-02-17 02:09:43 +0600

    js done

--- js/app.js
+++ js/app.js
@@ -2,11 +2,14 @@ function totalExpense() {
     const foodText = document.getElementById('food').value;
     const food = parseFloat(foodText);
 
+
     const rentText = document.getElementById('rent').value;
     const rent = parseFloat(rentText);
 
+
     const clothText = document.getElementById('cloth').value;
     const cloth = parseFloat(clothText);
+
 
     if (typeof food == "number" && typeof rent == "number" && typeof cloth == "number") {
         if (food > 0 && rent > 0 && cloth > 0) {
@@ -29,10 +32,11 @@ function balance() {
     const salary = parseFloat(salaryText);
     if (typeof salary == "number") {
         if (salary > 0) {
-            document.getElementById('alert').innerText = "";
+            document.getElementById('alert').innerText = '';
             const balance = document.getElementById('balance');
             const restMoney = salary - totalExpense();
             balance.innerText = restMoney;
+            return restMoney;
         } else {
             document.getElementById('alert').innerText = "Give number greater than 0";
         }
@@ -41,15 +45,35 @@ function balance() {
     }
 }
 
+function percentageAmount() {
+    const savePercentageText = document.getElementById('save-percentage');
+    const savePercentage = parseFloat(savePercentageText.value);
+    if (typeof savePercentage == "number" && savePercentage > 0) {
+        document.getElementById('second-alert').innerText = '';
+        const percentageAmount = savePercentage / 100;
+        const salaryText = document.getElementById('salary').value;
+        const salary = parseFloat(salaryText);
+        const saveAmount = salary * percentageAmount;
+        const saveAmountText = document.getElementById('save-amount');
+        saveAmountText.innerText = saveAmount;
+        return saveAmount;
+    } else {
+        document.getElementById('second-alert').innerText = "Invalid Input";
+    }
+}
+
+function remainingBalance() {
+    const remainingBalance = document.getElementById('rest-balance');
+    const restBalance = balance() - percentageAmount();
+    remainingBalance.innerText = restBalance;
+}
+
 document.getElementById('calculate-button').addEventListener('click', function() {
     totalExpense();
     balance();
 })
 
 document.getElementById('save').addEventListener('click', function() {
-    const savePercentageText = document.getElementById('save-percentage');
-    const savePercentage = parseFloat(savePercentageText.value);
-
-    const salary = document.getElementById('salary');
-
+    percentageAmount();
+    remainingBalance();
 })
