5fd4779f42116849b1e5e323c2e6b993555799cb
meraj191-35-2735 <www.shamsad35-2735@diu.edu.bd>
2022-02-17 13:29:45 +0600

    bonus error handler added

--- js/app.js
+++ js/app.js
@@ -1,22 +1,27 @@
-// Function for total Expense
+function makeUserInputBlanked(inputAmount) {
+    const inputText = document.getElementById(inputAmount);
+    inputText.value = '';
+}
+// function for making user Input to Number
+function userInput(input) {
+    const inputText = document.getElementById(input).value;
+    const inputAmount = parseFloat(inputText);
+    return inputAmount;
+}
+// function for total Expense
 function totalExpense() {
-    const foodText = document.getElementById('food').value;
-    const food = parseFloat(foodText);
-
-    const rentText = document.getElementById('rent').value;
-    const rent = parseFloat(rentText);
-
-    const clothText = document.getElementById('cloth').value;
-    const cloth = parseFloat(clothText);
-
-    const salaryText = document.getElementById('salary').value;
-    const salary = parseFloat(salaryText);
-
+    const food = userInput('food'); //call userInput function food input field
+    const rent = userInput('rent'); //call userInput function rent input field
+    const cloth = userInput('cloth'); //call userInput function cloth input field
+    const salary = userInput('salary'); //call userInput function salary input field
+    // error handler for if inputAmount is not a number
     if (typeof food == "number" && typeof rent == "number" && typeof cloth == "number") {
+        // error handler for if inputAmount is not a positive number
         if (food > 0 && rent > 0 && cloth > 0) {
             document.getElementById('alert').innerText = "";
             const expenses = document.getElementById('total-expense');
             const totalExpense = food + rent + cloth;
+            // error handler for expenses greater than salary
             if (totalExpense <= salary) {
                 expenses.innerText = totalExpense;
             } else {
@@ -33,9 +38,10 @@ function totalExpense() {
 
 // function for Balance
 function balance() {
-    const salaryText = document.getElementById('salary').value;
-    const salary = parseFloat(salaryText);
+    const salary = userInput('salary'); //call userInput function salary input field
+    // error handler for if salary is not a number
     if (typeof salary == "number") {
+        // error handler for if salary is not a positive number
         if (salary > 0) {
             document.getElementById('alert').innerText = '';
             const balance = document.getElementById('balance');
@@ -52,18 +58,20 @@ function balance() {
 
 // function for identify saving amount of salary
 function percentageAmount() {
-    const savePercentageText = document.getElementById('save-percentage');
-    const savePercentage = parseFloat(savePercentageText.value);
+    const savePercentage = userInput('save-percentage'); //call userInput function saving percentage input field
+    // error handler for if saving percentage is not a number and not a positive number
     if (typeof savePercentage == "number" && savePercentage > 0) {
         document.getElementById('second-alert').innerText = '';
         const percentageAmount = savePercentage / 100;
-        const salaryText = document.getElementById('salary').value;
-        const salary = parseFloat(salaryText);
+        const salary = userInput('salary'); //call userInput function salary input field
         const saveAmount = salary * percentageAmount;
         const saveAmountText = document.getElementById('save-amount');
+        // error handler for if saving amount greater than balance
         if (saveAmount <= balance()) {
             saveAmountText.innerText = saveAmount;
+            return saveAmount;
         } else {
+            saveAmountText.innerText = '';
             document.getElementById('second-alert').innerText = 'Insufficient Balance!';
         }
         return saveAmount;
@@ -75,8 +83,12 @@ function percentageAmount() {
 // function for Remaining Balance
 function remainingBalance() {
     const remainingBalance = document.getElementById('rest-balance');
-    const restBalance = balance() - percentageAmount();
-    remainingBalance.innerText = restBalance;
+    if (percentageAmount() <= balance()) {
+        const restBalance = balance() - percentageAmount();
+        remainingBalance.innerText = restBalance;
+    } else {
+        remainingBalance.innerText = '';
+    }
 }
 
 // add event handler to calculate button
@@ -89,4 +101,9 @@ document.getElementById('calculate-button').addEventListener('click', function()
 document.getElementById('save').addEventListener('click', function() {
     percentageAmount(); //call the percentageAmount function
     remainingBalance(); //call the remaining balance function
+    makeUserInputBlanked('food');
+    makeUserInputBlanked('rent');
+    makeUserInputBlanked('cloth');
+    makeUserInputBlanked('salary');
+    makeUserInputBlanked('save-percentage');
 })
